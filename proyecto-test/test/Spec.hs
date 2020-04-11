559b03faac05f6f880ed3d0651f3865ad4da2166
BrianTaylor <taylor.a.brian@gmail.com>
2020-04-11 12:12:28 -0300

    se agrega test de cuadruple

--- proyecto-test/test/Spec.hs
+++ proyecto-test/test/Spec.hs
@@ -8,3 +8,6 @@ main = hspec $ do
     it "El pdepreludat se instaló correctamente" $ do
       2 `shouldBe` doble 1
 
+  describe "Test cuadruple" $ do
+    it "cuadruple funciona correctamente" $ do
+      8 `shouldBe` cuadruple 2
